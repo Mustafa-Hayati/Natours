48443f666067afa6943735f19b23adb26d92449e
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-06-18 20:56:38 +0430

    Refactor more. Move routes to their files an d controllers to their own files.

---
+++ controllers/tourController.js
@@ -0,0 +1,115 @@
+const fs = require("fs");
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  // you can make a parameter optional by adding
+  // question mark (?) after it like :id?
+  const { id } = req.params;
+
+  const tour = tours.find(t => t.id === +id);
+  if (!tour)
+    return res.status(404).json({
+      status: "fail",
+      data: {
+        msg: "There's no such a tour.",
+      },
+    });
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  let { id } = req.params;
+  id = +id;
+
+  const tourIndex = tours.findIndex(t => t.id === id);
+  if (tourIndex === -1)
+    return res.status(404).json({
+      status: "fail",
+      data: {
+        msg: "There's no such a tour",
+      },
+    });
+
+  const updatedTour = {
+    ...tours[tourIndex],
+    ...req.body,
+  };
+
+  tours[tourIndex] = updatedTour;
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(200).json({
+        status: "success",
+        data: {
+          tour: updatedTour,
+        },
+      });
+    }
+  );
+};
+
+exports.deleteTour = (req, res) => {
+  let { id } = req.params;
+  id = +id;
+
+  const tourIndex = tours.findIndex(t => t.id === id);
+  if (tourIndex === -1)
+    return res.status(404).json({
+      status: "fail",
+      data: {
+        msg: "There's no such a tour",
+      },
+    });
+
+  tours.splice(tourIndex, 1);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(204).json({
+        status: "success",
+        data: {
+          tour: null,
+        },
+      });
+    }
+  );
+};
---
+++ controllers/userController.js
@@ -0,0 +1,44 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "success",
+    data: {
+      msg: "This route is yet to be implemented.",
+    },
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: "success",
+    data: {
+      msg: "This route is yet to be implemented.",
+    },
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: "success",
+    data: {
+      msg: "This route is yet to be implemented.",
+    },
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: "success",
+    data: {
+      msg: "This route is yet to be implemented.",
+    },
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "success",
+    data: {
+      msg: "This route is yet to be implemented.",
+    },
+  });
+};
