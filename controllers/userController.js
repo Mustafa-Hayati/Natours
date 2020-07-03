74891d57d4dca291e19b78cff013918c3a3e8d21
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-07-03 18:48:39 +0430

    Create login route and send a json web token to the client if the credentials are valid.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,17 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
+const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  // User.find().sort().select().skip().limit
+  res.status(200).json({
     status: "success",
+    results: users.length,
     data: {
-      msg: "This route is yet to be implemented.",
+      users,
     },
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
