ca5516abee0a3daea8884458b5499d862613e96f
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-07-04 20:20:02 +0430

    Data sanitization using express-mongo-sanitize and xss-clean packages. The first one is for NoSQL injection and the second one is used for decode html parts.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -36,6 +38,12 @@ app.use(
     limit: "10kb",
   })
 );
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// data sanitization against XSS attacks
+app.use(xss());
+
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
