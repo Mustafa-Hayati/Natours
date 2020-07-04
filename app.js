49355ec95c7edb374bdf78b7fc72ce612937d4c0
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-07-04 20:04:09 +0430

    Set security http headers using helmet.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -9,7 +10,10 @@ const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
+// Set security http headers
+app.use(helmet());
 
+// development logging
 if (process.env.NODE_ENV === "development") {
   console.log(`It's dev environment baby`);
   // console.log(JSON.stringify(process.env.NODE_ENV));
@@ -17,16 +21,22 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 3600000, // 1 hour
   message:
     "Too many requests from this IP, please try again in an hour.",
 });
-
 app.use("/api", limiter);
 
-app.use(express.json());
+// body parser, reading data from body, into req.body
+app.use(
+  express.json({
+    limit: "10kb",
+  })
+);
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", tourRouter);
