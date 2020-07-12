ae99c4cf2af8e5beecff2f6860e0df82814cab85
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-07-12 20:23:59 +0430

    Embed location models (geospatial data) in tourModel

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -25,7 +25,7 @@ mongoose
 // Read JSON file
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/tours.json`, "utf-8")
 );
 
 // Import data into the database;
