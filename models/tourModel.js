dcce60402cd64c6963e9cc6b78fbdc24d55b487d
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-07-12 22:19:51 +0430

    Change tour guides model to use child referencing in tourModel.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
-const User = require("./userModel");
+// const User = require("./userModel");
 // const validator = require("validator");
 
 const tourSchema = new mongoose.Schema(
@@ -114,7 +114,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -136,14 +141,14 @@ tourSchema.pre("save", function (next) {
   next();
 });
 
-tourSchema.pre("save", async function (next) {
-  const guidesPromises = this.guides.map(
-    async id => await User.findById(id)
-  );
+// tourSchema.pre("save", async function (next) {
+//   const guidesPromises = this.guides.map(
+//     async id => await User.findById(id)
+//   );
 
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 // tourSchema.post("save", function (doc, next) {
 //   console.log(doc);
