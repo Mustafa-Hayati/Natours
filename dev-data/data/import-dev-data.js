566603b1392b1b317936d0f23569c36969cf1be8
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-07-14 03:19:46 +0430

    Importing review and user data to import-dev-data module.

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const Tour = require("../../models/tourModel");
+const User = require("../../models/userModel");
+const Review = require("../../models/reviewModel");
 
 //! MongoDB Atlas
 // const DB = process.env.DATABASE.replace(
@@ -27,12 +29,25 @@ mongoose
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/tours.json`, "utf-8")
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/users.json`, "utf-8")
+);
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
+);
 
 // Import data into the database;
+//! make sure to comment pre save middlewares
+//! in userModel before running --import.
+//! Uncomment them afterwards.
+
+//? password for all users is test1234
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log("Data successfully loaded.");
   } catch (error) {
     console.log(error);
@@ -44,6 +59,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data successfully deleted.");
   } catch (error) {
     console.log(error);
