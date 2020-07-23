4bd68771bce2056c669cf4341b4ffb46c318d1bf
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-07-23 22:00:18 +0430

    Add image upload form to account page, take the photo, upload it, resize it and save it.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -28,6 +28,7 @@ const handleJWTExpiredError = () =>
   new AppError("Your token has expired, please log in again", 401);
 
 const sendErrorDev = (err, req, res) => {
+  // A) API
   if (req.originalUrl.startsWith("/api")) {
     return res.status(err.statusCode).json({
       status: err.status,
@@ -35,48 +36,53 @@ const sendErrorDev = (err, req, res) => {
       message: err.message,
       stack: err.stack,
     });
-  } else {
-    return res.status(err.statusCode).render("error", {
-      title: "Something went wrong",
-      msg: err.message,
-    });
   }
+
+  // B) RENDERED WEBSITE
+  console.error("ERROR 💥", err);
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong!",
+    msg: err.message,
+  });
 };
 
 const sendErrorProd = (err, req, res) => {
+  // A) API
   if (req.originalUrl.startsWith("/api")) {
+    // A) Operational, trusted error: send message to client
     if (err.isOperational) {
-      // Operational, trusted errors: send message to client
       return res.status(err.statusCode).json({
         status: err.status,
         message: err.message,
       });
-    } else {
-      //Programming or other unknown error: don't leak error detais
-      console.error("ERROR 🔥 :", err);
-
-      return res.status(500).json({
-        status: "error",
-        message: "Something went wrong",
-      });
     }
-  } else {
-    if (err.isOperational) {
-      // Operational, trusted errors: send message to client
-      return res.status(err.statusCode).render("error", {
-        title: "Something went wrong",
-        msg: err.message,
-      });
-    } else {
-      //Programming or other unknown error: don't leak error detais
-      console.error("ERROR 🔥 :", err);
-
-      return res.status(err.statusCode).render("error", {
-        title: "Something went wrong",
-        msg: "Please try again later",
-      });
-    }
+    // B) Programming or other unknown error: don't leak error details
+    // 1) Log error
+    console.error("ERROR 💥", err);
+    // 2) Send generic message
+    return res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
   }
+
+  // B) RENDERED WEBSITE
+  // A) Operational, trusted error: send message to client
+  if (err.isOperational) {
+    console.log(err);
+    return res.status(err.statusCode).render("error", {
+      title: "Something went wrong!",
+      msg: err.message,
+    });
+  }
+  // B) Programming or other unknown error: don't leak error details
+  // 1) Log error
+  console.error("ERROR 💥", err);
+  // 2) Send generic message
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong!",
+    msg: "Please try again later.",
+  });
 };
 
 module.exports = (err, req, res, next) => {
@@ -88,8 +94,6 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === "production") {
-    // let error = { ...err, errmsg: msg };
-    // let error = JSON.parse(JSON.stringify(err));
     let error = cloneDeep(err);
 
     if (error.name === "CastError") error = handleCastErrorDB(error);
